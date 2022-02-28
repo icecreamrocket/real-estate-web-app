f18a2a2e3689987a3493961cafed9715f679443c
Hagen <57143868+icecreamrocket@users.noreply.github.com>
2022-02-28 21:23:45 +0800

    Started For Sale and For Rent (FE)

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Link from "next.link";
+import Image from "next/image";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+
+// Banner Component
+
+const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  linkName,
+  imageUrl,
+}) => <Flex></Flex>;
+
+export default function Home() {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
